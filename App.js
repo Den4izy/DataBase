a2c71c0adc791525b3916d8cf74763cb76d935af
Denys Demydenko <kawoonne@gmail.com>
2022-03-29 13:19:27 +0300

    coments

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
+// підключаємо модуль
 const mysql = require('mysql');
 
-
+// конфігурація підключення
 const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'mybase',
-    password: ''
+    host: 'localhost', // адрес бази(www.site.com)
+    user: 'root', // користувач бази
+    database: 'mybase', // назва бази
+    password: '' // пароль від бази(якщо нема то залишаємо пусту строку)
 });
 
+// робимо підключення з базою
 conn.connect(err =>{
     if(err){
         console.log(err);
@@ -18,14 +20,15 @@ conn.connect(err =>{
     }
 });
 
+// робимо запрос
 let query = "SELECT * FROM testtable";
 conn.query(query, (err, result, field) =>{
-    console.log(err);
-    console.log(result);
-    //console.log(field);
-
+    console.log(err); // якщо все ок то виводить NULL
+    console.log(result); // в результат виводиться звичайний асоціативний масів
+    //console.log(field); // дані полів таблиці
 });
 
+// закриваємо з*єднання з базою
 conn.end(err=>{
     if(err){
         console.log(err);
